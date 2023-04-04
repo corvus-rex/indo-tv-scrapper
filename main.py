884581a88c9a557d531cb32c2f1b066b2e819a33
Adrianov <adrianovriadi@gmail.com>
2023-04-04 14:52:31 +0700

    Added scheduler

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import os
 from datetime import datetime
 from pathlib import Path    
 import json
+import schedule
 
 
 def filter_by_channel(channel):
     url = "https://iptv-org.github.io/epg/guides/id/vidio.com.xml"
     ts = time.time()
+    print("Performing fetch request at " + str(ts) + "...")
     response = requests.get(url)
     data = xmltodict.parse(response.content)
     programs = data['tv']['programme']
@@ -38,5 +40,15 @@ def filter_by_channel(channel):
     with open(os.path.join(full_path, file_name), 'w') as fp:
         json.dump(new_json, fp, indent=3)
 
+def fetch_all(query):
+    for e in query:
+        filter_by_channel(e)
+
 if __name__ == "__main__":
-    filter_by_channel("KompasTV.id")
+    query = ["KompasTV.id"]
+    fetch_all(query)
+    
+    schedule.every(720).minutes.do(fetch_all, query)
+    while(True):
+        schedule.run_pending()
+        time.sleep(1)
